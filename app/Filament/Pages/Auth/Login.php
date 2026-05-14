19f3984d6e815b61464dde98cc5c005abd6aafbf
Eko Muchamad Haryono <66320809+ekomh170@users.noreply.github.com>
2026-05-15 01:22:28 +0700

    feat: rapikan halaman login - nama perusahaan dari setting & label form

--- app/Filament/Pages/Auth/Login.php
+++ app/Filament/Pages/Auth/Login.php
@@ -2,7 +2,12 @@
 
 namespace App\Filament\Pages\Auth;
 
+use App\Models\Setting;
+use Filament\Forms\Components\Checkbox;
+use Filament\Forms\Components\TextInput;
 use Filament\Notifications\Notification;
+use Filament\Schemas\Components\Component;
+use Illuminate\Contracts\Support\Htmlable;
 
 class Login extends \Filament\Auth\Pages\Login
 {
@@ -19,5 +24,41 @@ class Login extends \Filament\Auth\Pages\Login
 
             session()->forget('logout_success');
         }
+    }
+
+    public function getHeading(): string | Htmlable | null
+    {
+        return Setting::get('nama_perusahaan', config('app.name'));
+    }
+
+    public function getSubheading(): string | Htmlable | null
+    {
+        return 'Masuk ke akun Anda';
+    }
+
+    protected function getEmailFormComponent(): Component
+    {
+        return TextInput::make('email')
+            ->label('Email')
+            ->email()
+            ->required()
+            ->autocomplete()
+            ->autofocus();
+    }
+
+    protected function getPasswordFormComponent(): Component
+    {
+        return TextInput::make('password')
+            ->label('Kata Sandi')
+            ->password()
+            ->revealable(filament()->arePasswordsRevealable())
+            ->autocomplete('current-password')
+            ->required();
+    }
+
+    protected function getRememberFormComponent(): Component
+    {
+        return Checkbox::make('remember')
+            ->label('Ingat saya');
     }
 }
